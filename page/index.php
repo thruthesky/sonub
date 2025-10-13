3c8a301afcbb16969afea60c88d28baa7cc5e3e2
JaeHo Song <thruthesky@gmail.com>
2025-10-13 22:59:00 +0900

    Commit for the release of the version: 2025-10-13-22-59-00

--- page/index.php
+++ page/index.php
@@ -9,11 +9,7 @@ inject_index_language();
     <strong><?php echo t()->{'Browser Language'}; ?>:</strong> <?php echo get_browser_language(); ?>
 </div>
 
-<h1 class="welcome-text">Welcome,</h1>
-
-<p>
-    Nickname: <?php echo login()->display_name ?? 'Guest'; ?><br>
-</p>
+<h1 class="welcome-text"><?= t()->환영합니다 ?>, <?= login()->display_name ?? t()->손님 ?></h1>
 
 
 <div class="card mt-4">
@@ -29,12 +25,14 @@ inject_index_language();
 function inject_index_language()
 {
     t()->inject([
-        'Welcome to Sonub' => ['en' => 'Welcome to Sonub', 'ko' => 'Sonub에 오신 것을 환영합니다', 'ja' => 'Sonubへようこそ', 'zh' => '欢迎使用Sonub'],
-        'Sonub application is running!' => ['en' => 'Sonub application is running!', 'ko' => 'Sonub 애플리케이션이 실행 중입니다!', 'ja' => 'Sonubアプリケーションが実行中です！', 'zh' => 'Sonub应用程序正在运行！'],
-        'Browser Language' => ['en' => 'Browser Language', 'ko' => '브라우저 언어', 'ja' => 'ブラウザ言語', 'zh' => '浏览器语言'],
-        'This is a sample application using Firebase Phone Authentication and a MySQL database.' => ['en' => 'This is a sample application using Firebase Phone Authentication and a MySQL database.', 'ko' => '이것은 Firebase 전화 인증과 MySQL 데이터베이스를 사용하는 샘플 애플리케이션입니다.', 'ja' => 'これはFirebase電話認証とMySQLデータベースを使用したサンプルアプリケーションです。', 'zh' => '这是一个使用Firebase电话认证和MySQL数据库的示例应用程序。'],
-        'Getting Started' => ['en' => 'Getting Started', 'ko' => '시작하기', 'ja' => 'はじめに', 'zh' => '入门指南'],
-        'Start building your application with our powerful features.' => ['en' => 'Start building your application with our powerful features.', 'ko' => '강력한 기능으로 애플리케이션 구축을 시작하세요.', 'ja' => '強力な機能でアプリケーションの構築を開始しましょう。', 'zh' => '使用我们的强大功能开始构建您的应用程序。'],
-        'Learn More' => ['en' => 'Learn More', 'ko' => '더 알아보기', 'ja' => 'もっと詳しく', 'zh' => '了解更多'],
+        'Welcome to Sonub' => ['ko' => 'Sonub에 오신 것을 환영합니다', 'en' => 'Welcome to Sonub', 'ja' => 'Sonubへようこそ', 'zh' => '欢迎使用Sonub'],
+        'Sonub application is running!' => ['ko' => 'Sonub 애플리케이션이 실행 중입니다!', 'en' => 'Sonub application is running!', 'ja' => 'Sonubアプリケーションが実行中です！', 'zh' => 'Sonub应用程序正在运行！'],
+        'Browser Language' => ['ko' => '브라우저 언어', 'en' => 'Browser Language', 'ja' => 'ブラウザ言語', 'zh' => '浏览器语言'],
+        'This is a sample application using Firebase Phone Authentication and a MySQL database.' => ['ko' => '이것은 Firebase 전화 인증과 MySQL 데이터베이스를 사용하는 샘플 애플리케이션입니다.', 'en' => 'This is a sample application using Firebase Phone Authentication and a MySQL database.', 'ja' => 'これはFirebase電話認証とMySQLデータベースを使用したサンプルアプリケーションです。', 'zh' => '这是一个使用Firebase电话认证和MySQL数据库的示例应用程序。'],
+        'Getting Started' => ['ko' => '시작하기', 'en' => 'Getting Started', 'ja' => 'はじめに', 'zh' => '入门指南'],
+        'Start building your application with our powerful features.' => ['ko' => '강력한 기능으로 애플리케이션 구축을 시작하세요.', 'en' => 'Start building your application with our powerful features.', 'ja' => '強力な機能でアプリケーションの構築を開始しましょう。', 'zh' => '使用我们的强大功能开始构建您的应用程序。'],
+        'Learn More' => ['ko' => '더 알아보기', 'en' => 'Learn More', 'ja' => 'もっと詳しく', 'zh' => '了解更多'],
+        '환영합니다' => ['ko' => '환영합니다', 'en' => 'Welcome', 'ja' => 'ようこそ', 'zh' => '欢迎'],
+        '손님' => ['ko' => '손님', 'en' => 'Guest', 'ja' => 'ゲスト', 'zh' => '访客'],
     ]);
 }
