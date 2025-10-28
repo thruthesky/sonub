efeade2957c2d52db17a3b4e01d9ca046fa2e90a
JaeHo Song <thruthesky@gmail.com>
2025-10-28 14:25:49 +0900

    feat: add description for update_my_profile endpoint in API configuration

--- etc/config/api.config.php
+++ etc/config/api.config.php
@@ -23,6 +23,7 @@ class AppConfigApi
             'file_upload' => '파일 업로드',
             'file_delete' => '파일 삭제',
             'get_post' => '게시물 가져오기. 게시글 하나를 가져올 때 사용합니다. 추가적으로 with_users: boolean, with_comments: boolean 옵션으로 사용자 정보와 코멘트 정보를 같이 올 수 있습니다.',
+            'update_my_profile' => 'Update the profile of the currently logged-in user.',
         ];
     }
 
